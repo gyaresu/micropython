9adcb15273b6e1d460b6f67f159115ef94f6e757
Gareth <me@gareth.codes>
2016-07-21 19:01:20 -0700

    teensy scripts

--- teensy/memzip_files/main.py
+++ teensy/memzip_files/main.py
@@ -1,13 +1,19 @@
+import pyb
 print("Executing main.py")
 
 led = pyb.LED(1)
 
-led.on()
-pyb.delay(100)
-led.off()
-pyb.delay(100)
-led.on()
-pyb.delay(100)
-led.off()
+def blinken(n):
+    for m in range(1, n):
+        led.on()
+        pyb.delay(100)
+        led.off()
+        pyb.delay(100)
+        led.on()
+        pyb.delay(100)
+        led.off()
+    print("End of script")
+
+
 
 
